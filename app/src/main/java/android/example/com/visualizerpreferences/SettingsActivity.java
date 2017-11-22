b1b3b3a20b079a6ead85063a822e8f6b5676a04b
Melih Gültekin <melih.gultekin@d-teknoloji.com.tr>
2017-11-22 20:58:23 +0300

    TODO done

---
+++ app/src/main/java/android/example/com/visualizerpreferences/SettingsActivity.java
@@ -0,0 +1,36 @@
+package android.example.com.visualizerpreferences;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @RequiresApi(api = Build.VERSION_CODES.HONEYCOMB)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar actionBar = this.getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home: {
+                NavUtils.navigateUpFromSameTask(SettingsActivity.this);
+                return true;
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
