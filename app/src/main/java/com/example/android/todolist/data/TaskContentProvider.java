7d9bb2cdef8a5f33b023c43f08c6062235392cec
Melih Gültekin <mmelihgultekin@gmail.com>
2017-12-13 15:10:21 +0300

    TODO done

--- app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
+++ app/src/main/java/com/example/android/todolist/data/TaskContentProvider.java
@@ -31,7 +31,7 @@ public class TaskContentProvider extends ContentProvider {
     // It's convention to use 100, 200, 300, etc for directories,
     // and related ints (101, 102, ..) for items in that directory.
     public static final int TASKS = 100;
-    public static final int TASKS_WITH_ID = 101;
+    public static final int TASK_WITH_ID = 101;
 
     // COMPLETED (3) Declare a static variable for the Uri matcher that you construct
     private static final UriMatcher uriMatcher = buildUriMatcher();
@@ -40,7 +40,7 @@ public class TaskContentProvider extends ContentProvider {
     public static UriMatcher buildUriMatcher() {
         UriMatcher uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS, TASKS);
-        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASKS_WITH_ID);
+        uriMatcher.addURI(TaskContract.AUTHORITY, TaskContract.PATH_TASKS + "/#", TASK_WITH_ID);
         return uriMatcher;
     }
 
